885f41946b9859b62aa861ebb3e35abd26f6737e
ahmadmhasann <ahmadmhasann@gmail.com>
2022-12-10 10:06:34 +0200

    First commit

---
+++ C_COURSE/lec1/lec1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+int main (void) {
+	/*
+		char x = 65.6;
+		char x;
+		int y = 'm';
+		float z = 65.7;
+		printf("x = %f\n", (float)x);
+		printf("y = %c\n", z);
+		printf("z = %d\n", (int)z);
+		printf("float z = %f\n", z);
+		*/
+	/*	
+	int x,y;
+	printf("Please enter x: ");
+	scanf("%d", &x);
+	printf("Please enter y: ");
+	scanf("%d", &y);
+	printf("%d + %d = %d\n", x, y, x+y);
+	printf("%d - %d = %d\n", x, y, x-y);
+	printf("%d * %d = %d\n", x, y, x*y);
+	printf("%d / %d = %d\n", x, y, x/y);
+	printf("%d %% %d = %d\n", x, y, x%y);
+	*/
+	//
+	/*  */
+	/*
+	int x = 181, y = 150;
+	printf("x & y = %x\n", x&y);
+	printf("x | y = %d\n", x|y);
+	printf("x ^ y = %d\n", x^y);
+	printf("~x = %d\n", ~x);
+	printf("~y = %d\n", ~y);
+	printf("x>>2 = %d\n", x>>2);
+	printf("y<<3 = %d\n", y<<3);
+	*/
+	
+	int x = 10, y = 7;
+	printf("x>y = %d\n", x>y);
+	printf("x<=y = %d\n", x<=y);
+	printf("x==y = %d\n", x==y);
+	printf("%d!=y = %d\n",x,  x!=y);
+
+}
