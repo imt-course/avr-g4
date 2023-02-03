0dc63a3617c3f9dfa4a02d508124ed02470071d8
Ahmed Hassan <ahmadmhasann@gmail.com>
2023-02-03 14:53:03 +0200

    fix Lcd

--- ECLIPSE_PROJECTS/AVR_DRIVERS/src/Lcd.c
+++ ECLIPSE_PROJECTS/AVR_DRIVERS/src/Lcd.c
@@ -60,14 +60,14 @@ void Lcd_SendCommand(u8 command) {
 void Lcd_SendData(u8 data) {
     Dio_SetPinLevel(LCD_PIN_RS, DIO_LEVEL_HIGH);
     Dio_SetPinLevel(LCD_PIN_RW, DIO_LEVEL_LOW);
-    Dio_SetPinLevel(LCD_PIN_D0, GET_BIT(command, 0));
-    Dio_SetPinLevel(LCD_PIN_D1, GET_BIT(command, 1));
-    Dio_SetPinLevel(LCD_PIN_D2, GET_BIT(command, 2));
-    Dio_SetPinLevel(LCD_PIN_D3, GET_BIT(command, 3));
-    Dio_SetPinLevel(LCD_PIN_D4, GET_BIT(command, 4));
-    Dio_SetPinLevel(LCD_PIN_D5, GET_BIT(command, 5));
-    Dio_SetPinLevel(LCD_PIN_D6, GET_BIT(command, 6));
-    Dio_SetPinLevel(LCD_PIN_D7, GET_BIT(command, 7));
+    Dio_SetPinLevel(LCD_PIN_D0, GET_BIT(data, 0));
+    Dio_SetPinLevel(LCD_PIN_D1, GET_BIT(data, 1));
+    Dio_SetPinLevel(LCD_PIN_D2, GET_BIT(data, 2));
+    Dio_SetPinLevel(LCD_PIN_D3, GET_BIT(data, 3));
+    Dio_SetPinLevel(LCD_PIN_D4, GET_BIT(data, 4));
+    Dio_SetPinLevel(LCD_PIN_D5, GET_BIT(data, 5));
+    Dio_SetPinLevel(LCD_PIN_D6, GET_BIT(data, 6));
+    Dio_SetPinLevel(LCD_PIN_D7, GET_BIT(data, 7));
     Dio_SetPinLevel(LCD_PIN_EN, DIO_LEVEL_HIGH);
     _delay_ms(2);
     Dio_SetPinLevel(LCD_PIN_EN, DIO_LEVEL_LOW);
