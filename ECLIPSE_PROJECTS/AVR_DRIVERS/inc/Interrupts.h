2fa3516073d592ab0432178b4815d6e9ab7927b7
Ahmed Hassan <ahmadmhasann@gmail.com>
2023-02-17 12:00:37 +0200

    fix vector

--- ECLIPSE_PROJECTS/AVR_DRIVERS/inc/Interrupts.h
+++ ECLIPSE_PROJECTS/AVR_DRIVERS/inc/Interrupts.h
@@ -8,9 +8,9 @@
 #ifndef INC_INTERRUPTS_H_
 #define INC_INTERRUPTS_H_
 
-#define VECTOR_INT0 __vetor_1
-#define VECTOR_INT1 __vetor_2
-#define VECTOR_INT2 __vetor_3
+#define VECTOR_INT0 __vector_1
+#define VECTOR_INT1 __vector_2
+#define VECTOR_INT2 __vector_3
 
 
 #define ISR(vector) void vector (void) __attribute__((signal));\
